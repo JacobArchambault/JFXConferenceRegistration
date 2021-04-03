e375308977644bc889a683fab54c7d7ae69e0bdf
Jacob Archambault <jacob.archambault@waystar.com>
2021-04-02 20:20:18 -0400

    Add radio buttons to HBox.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module org.jacobarchambault.conferenceregistration {
     requires javafx.controls;
+	requires javafx.graphics;
     exports org.jacobarchambault.conferenceregistration;
 }
--- src/main/java/org/jacobarchambault/conferenceregistration/App.java
+++ src/main/java/org/jacobarchambault/conferenceregistration/App.java
@@ -3,6 +3,8 @@ package org.jacobarchambault.conferenceregistration;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -15,7 +17,13 @@ public class App extends Application {
 	@Override
 	public void start(Stage stage) {
 
-		stage.setScene(new Scene(new HBox(new Label("Registration type: "))));
+		stage
+				.setScene(
+						new Scene(
+								new HBox(
+										new Label("Registration type: "),
+										new RadioButton("General registration, $895"),
+										new RadioButton("Student Registration, $495"))));
 		stage.show();
 	}
 
