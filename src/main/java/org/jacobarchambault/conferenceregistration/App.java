95aff4fcc422fc39080c740ed16f86c92daf9c27
Jacob Archambault <jacob.archambault@waystar.com>
2021-04-02 20:14:33 -0400

    Change main element to HBox with registration type label

--- src/main/java/org/jacobarchambault/conferenceregistration/App.java
+++ src/main/java/org/jacobarchambault/conferenceregistration/App.java
@@ -3,6 +3,7 @@ package org.jacobarchambault.conferenceregistration;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,15 +15,7 @@ public class App extends Application {
 	@Override
 	public void start(Stage stage) {
 
-		stage
-				.setScene(
-						new Scene(
-								new StackPane(
-										new Label(
-												"Hello, JavaFX " + SystemInfo.javafxVersion() + ", running on Java "
-														+ SystemInfo.javaVersion() + ".")),
-								640,
-								480));
+		stage.setScene(new Scene(new HBox(new Label("Registration type: "))));
 		stage.show();
 	}
 
