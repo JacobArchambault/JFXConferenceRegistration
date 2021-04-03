068f16276638a973a0ea4cfd7e52c2330ae484b9
Jacob Archambault <jacob.archambault@waystar.com>
2021-04-02 21:21:08 -0400

    Add eventButton class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.jacobarchambault.conferenceregistration {
 	requires javafx.controls;
 	requires javafx.graphics;
+	requires javafx.base;
 
 	exports org.jacobarchambault.conferenceregistration;
 }
